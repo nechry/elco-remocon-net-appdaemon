1de337716170b84ace865ed64b94f627f3f05b7d
nechry <nechry@gmail.com>
2023-04-21 09:27:12 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # elco-remocon-net-appdaemon
-Elco Remocon.net AppDaemon app for homeassistant
+
+ELCO Remocon.net AppDaemon app to retrieve data from the gas boiler system via the Elco Remocon-Net cloud service and push them to home-assistant entities.
+
+This appdaemon will go fetch your gas data from ELCO Remocon.net's portal, which you would find at https://www.remocon-net.remotethermo.com
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-41BDF5.svg?style=for-the-badge)](https://github.com/hacs/integration)
+
+# HACS configuration
+Make sure that you [have the AppDaemon discovery and tracking](https://hacs.xyz/docs/categories/appdaemon_apps) enabled for HACS.
+
+## Installation
+
+### AppDaemon's python packages pre-requisites
+
+No additional python packages are required.
+
+## Configuration
+
+### secrets.yaml
+You will need the following in your secrets.yaml file
+
+```yaml
+username: <YOUR_EMAIL>
+password: <YOUR_PASSWORD>
+gateway_id: "<YOUR_INSTALLATION_NUMBER>"
+bearer_token: <HA_LONG_LIVE_TOKEN>
+```
+
+The `bearer_token` refers to a long-lived Home Assistant token.
+
+### apps.yaml
+
+Define your app like the following:
+
+```yaml
+remocon:
+  module: elco-remocon-net-appdaemon
+  class: Remocon
+  plugin: HASS
+  base_url: https://www.remocon-net.remotethermo.com
+  username: !secret remocon_username
+  password: !secret remocon_password
+  bearer_token: !secret remocon_bearer_token
+  gateway_id: !secret remocon_gateway_id
+  # refresh_rate: 60 # optional
+  #ha_url:  # optional, in case hassplugin ha_url undefined
+```
+
+## Usage
+
+POSTs the data to the following two new HA sensors:
+
+- `sensor.elco_outside_temperature` Elco Outside Temperature, plantData.outsideTemp
+- `sensor.elco_domestic_hot_water_storage_temperature`: Elco Domestic Hot Water Storage Temperature, plantData.dhwStorageTemp
+- `sensor.elco_domestic_hot_water_temperature`: Elco Domestic Hot Water Temperature, plantData.dhwComfortTemp.value
+- `sensor.elco_domestic_hot_water_storage_mode`: Elco Domestic Hot Water Storage Mode, plantData.dhwMode.value
+- `sensor.elco_comfort_room_temperature_setpoint`: Elco Comfort room Temperature setpoint, zoneData.chComfortTemp.value
+- `sensor.elco_reduced_room_temperature_setpoint`: Elco Reduced room Temperature setpoint, zoneData.chReducedTemp.value
+- `sensor.elco_room_temperature`: Elco Room Temperature, zoneData.roomTemp
+- `sensor.elco_desired_room_temperature`: Elco Desired Room Temperature, zoneData.desiredRoomTemp
+- `sensor.elco_room_operation_mode_heating`: Elco Room Operation mode heating, zoneData.mode.value
+- `binary_sensor.elco_domestic_hot_water_storage_temperature_error`: Elco Domestic Hot Water Storage Temperature Error, plantData.dhwStorageTempError
+- `binary_sensor.elco_outside_temperature_error`: Elco Outside Temperature Error, plantData.outsideTempError
+- `binary_sensor.elco_heatpump_on`: Elco HeatPump On, plantData.heatPumpOn
+- `binary_sensor.elco_domestic_hot_water_enabled`: Elco Domestic Hot Water Enabled, plantData.dhwEnabled
+- `binary_sensor.elco_room_heating_is_active`: Elco Room Heating is Active, zoneData.isHeatingActive
+- `binary_sensor.elco_room_heating_is_request`: Elco Room Heating is Request, zoneData.heatOrCoolRequest
+
+## Limitations
+
+This appdaemon's app only supports 1 gateway.
+Readonly access, not set point, at the moment.
+
+## License
+
+[MIT](LICENSE)
---
+++ apps/remocon/remocon.py
@@ -0,0 +1,89 @@
+import appdaemon.plugins.hass.hassapi as hass
+import requests
+import json
+from urllib.parse import quote
+
+class Remocon(hass.Hass):
+    
+    def initialize(self):
+        refresh_rate = self.args.get("refresh_rate")
+        if(refresh_rate is None):
+            refresh_rate = 60        
+        self.log(f"Will fetch remocon.net data every {refresh_rate} min")
+        self.run_every(self.get_remocon_data, "now", refresh_rate * 60)
+    
+    def post_to_entities(self, data):
+        self.log("Posting to entities...")
+        
+        #self.log(json.dumps(data))        
+        def _post_plantData():
+            pass
+        
+        def _post_zoneData():
+            pass
+        
+        try:
+            if self.args["ha_url"]:
+                # get HA's url from app's first, if configured/overridden by user
+                self.log("Using ha_url from app's configuration")
+                ha_url = self.args["ha_url"]
+            else:
+                ha_url = self.config["plugins"]["HASS"]["ha_url"]
+        except Exception as e:
+            self.log(
+                "No Home Assistant URL could be found. Please configure ha_url in the app's configuration. Aborting."
+            )
+            self.log(e)
+            return
+        
+        _post_plantData()
+        _post_zoneData()
+     
+    def get_remocon_data(self, kwargs):
+        self.log("Fetching remocon data...")
+        try:
+            base_url = self.args.get("base_url")
+            gateway = self.args.get("gateway_id")
+            username = quote(self.args.get('username'), safe='')
+            password = quote(self.args.get('password'), safe='')
+        except Exception as config_e:
+            self.log("There was a problem getting configuration values. Aborting.")
+            return
+        try:
+            login_url = f"{base_url}/R2/Account/Login?returnUrl=HTTP/2"
+            payload = f'Email={username}&Password={password}&RememberMe=false'
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Cookie": "browserUtcOffset=-120"
+                }
+            session = requests.session()
+            # login,get session cookie and address for json data
+            response = session.post(url=login_url, headers=headers, data=payload)
+            result_json = json.loads(response.text)
+            if(result_json['ok']):
+                # get zone 1 data                
+                payload = {
+                    "useCache": True,
+                    "zone": 1,
+                    "filter":
+                        {
+                            "progIds": "null",
+                            "plant": True,
+                            "zone": True
+                        }
+                    }
+                data_url = f"{base_url}/R2/PlantHomeBsb/GetData/{gateway}"
+                response = session.post(url=data_url, json=payload)
+                if(response.status_code==200):
+                    result_json = json.loads(response.text)
+                    self.post_to_entities(result_json['data'])
+                    return
+                else:
+                    self.error(response.text)
+            else:
+                self.error(result_json['message'])
+            self.log("Done fetching remocon data.")
+        except Exception as e:
+            self.error(e)
+        finally:
+            pass
